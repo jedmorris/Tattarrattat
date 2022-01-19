dcc98c791f37723eb36b2a206b99029c4b832638
Jed Morris <jemorris110@gmail.com>
2022-01-19 14:45:38 -0800

    built out app and js.

--- js/site.js
+++ js/site.js
@@ -1,6 +1,6 @@
-// display message to the user
-function getMessage() {
-	let userMessage = document.getElementById("message").value;
+// test js functionality  
+function getTestMessage() {
+	let userMessage = document.getElementById("userString").value;
 	Swal.fire(
 		'Button Clicked!',
 		userMessage,
@@ -8,3 +8,35 @@ function getMessage() {
 	)
 }
 
+function getValues() {
+	let userStringInput = document.getElementById("userString").value;
+
+	// check if user input is string
+	if (typeof userStringInput === 'string') {
+	
+		let newString = reverseString(userStringInput);
+		displayUserString(newString);
+	} else {
+		Swal.fire({
+			icon: 'error',
+			title: 'Whoops...',
+			text: 'Only strings are allowed for Tattarrattat.'
+		})
+	}
+
+}
+
+function reverseString(userStringInput) {
+	let returnString = "";
+
+	// loop through user input and place chars in new array in reverse order
+	for (let i = userStringInput.length - 1; i >= 0; i--) {
+		returnString += userStringInput[i];
+	}
+	return returnString;
+}
+
+function displayString() {
+	
+
+}
